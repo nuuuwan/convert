d2595cac88aa1f9427b7b294dacf53baca0a832c
nuuuwan <nuuuwan@gmail.com>
2025-04-12 10:44:50 +0530

    dedicated copy folder

--- workflows/docx_to_md.py
+++ workflows/docx_to_md.py
@@ -12,14 +12,20 @@ def docx_to_md(docx_path, md_path):
     docx_doc = DocXDoc.from_file(docx_path)
     md_doc = MarkdownDoc.from_instance(docx_doc)
     md_doc.to_file(md_path)
-    log.info(f"Converted {docx_path} -> {md_path}")
+    log.debug(f"Converted {docx_path} -> {md_path}")
 
 
 def main(dir_path):
+    dir_path_md = dir_path + ".md"
+    os.makedirs(dir_path_md, exist_ok=True)
+    log.info(f"Created {dir_path_md}")
+
     for filename in os.listdir(dir_path):
         if filename.endswith(".docx"):
             docx_path = os.path.join(dir_path, filename)
-            md_path = os.path.splitext(docx_path)[0] + ".md"
+            md_path = os.path.join(
+                dir_path_md, filename.replace(".docx", ".md")
+            )
             docx_to_md(docx_path, md_path)
 
 
