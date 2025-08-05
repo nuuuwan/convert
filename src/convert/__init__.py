2f2ab9b315d2b5bde21ee5cae1ffc24cfd950090
nuuuwan <nuuuwan@gmail.com>
2025-08-05 07:17:34 +0530

    lint only

--- src/convert/__init__.py
+++ src/convert/__init__.py
@@ -1,4 +1,5 @@
 # convert (auto generate by build_inits.py)
+# flake8: noqa: F408
 
 from convert.core import (AbstractDoc, DocXDoc, MarkdownDoc, Paragraph,
                           StopWords, TexDoc)
--- src/convert/core/__init__.py
+++ src/convert/core/__init__.py
@@ -1,4 +1,5 @@
 # convert.core (auto generate by build_inits.py)
+# flake8: noqa: F408
 
 from convert.core.AbstractDoc import AbstractDoc
 from convert.core.DocXDoc import DocXDoc
