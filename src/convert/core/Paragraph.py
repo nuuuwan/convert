38f995ba3b51c4cec68c7913b2801f352a0a1ba5
nuuuwan <nuuuwan@gmail.com>
2025-08-05 08:18:40 +0530

    FIX CFQ004 Function "get_temp_audio_file_path" has 4 returns that exceeds max allowed 3

--- src/convert/core/Paragraph.py
+++ src/convert/core/Paragraph.py
@@ -84,16 +84,7 @@ class Paragraph:
     def lower(self):
         return Paragraph(Paragraph.get_lower_tag(self.tag), self.text)
 
-    def get_temp_audio_file_path(self, force=False):
-        if self.text.strip() in ["", "...", "---"]:
-            return None
-
-        file_path = os.path.join(
-            tempfile.gettempdir(), f"paragraph.{self.md5}.mp3"
-        )
-
-        if not force and os.path.exists(file_path):
-            return file_path
+    def __get_temp_audio_file_path_hot__(self, file_path):
 
         try:
             slow = False if self.tag == "p" else True
@@ -109,3 +100,15 @@ class Paragraph:
         except Exception as e:
             log.error(f'("{self.text}") {e}')
             return None
+
+    def get_temp_audio_file_path(self, force: bool = False):
+        if self.text.strip() in ["", "...", "---"]:
+            return None
+
+        file_path = os.path.join(
+            tempfile.gettempdir(), f"paragraph.{self.md5}.mp3"
+        )
+        if not force and os.path.exists(file_path):
+            return file_path
+
+        return self.__get_temp_audio_file_path_hot__(file_path)
